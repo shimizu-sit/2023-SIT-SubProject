898f1927857a2d9d6c073ca1c4d3bc38b5ff54a8
Shimizu Tetsuya <shimizu@info.shonan-it.ac.jp>
2023-06-19 08:10:23 +0900

    Update FragmentShader.glsl

--- 1st-week-Sample/Sample_01_001/data/FragmentShader.glsl
+++ 1st-week-Sample/Sample_01_001/data/FragmentShader.glsl
@@ -1,8 +1,11 @@
-// OpenGLのバージョンを指定
+// GLSLのバージョンを指定
 #version 400
+// floatの制度指定
 precision highp float;
+// 出力用のvec4型変数
 out vec4 fragColor;
 
 void main(){
+    // 出力色の設定(白色)
     fragColor=vec4(1.,1.,1.,1.);
 }
